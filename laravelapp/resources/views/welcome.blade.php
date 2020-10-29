f898b538e2883a672e43c9c2b500a0ba18074060
yuya mita <kwansei0411@gmail.com>
2020-10-29 11:12:28 +0900

    10/29

--- laravelapp/resources/views/welcome.blade.php
+++ laravelapp/resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
                 font-family: 'Nunito';
             }
         </style>
-    </head>
+    </head>a
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
