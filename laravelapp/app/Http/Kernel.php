f96ad011d4116a21fe14158f10909c3f0f9de4b0
yuya mita <kwansei0411@gmail.com>
2020-11-13 12:18:10 +0900

    更に修正

--- laravelapp/app/Http/Kernel.php
+++ laravelapp/app/Http/Kernel.php
@@ -46,6 +46,7 @@ class Kernel extends HttpKernel
 
         'mypage' => [
             App\Http\Middleware\Mypagemiddleware::class
+        ],
 
         'register' => [
             \App\Http\Middleware\RegisterMiddleware::class
